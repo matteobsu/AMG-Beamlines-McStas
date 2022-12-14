c6101fc97955f5b1eaf02e4c609e680ea3a7b01a
matteobsu <matteo.busi@psi.ch>
2022-12-14 10:31:09 +0100

    bla

--- BOA/PlotResults.m
+++ BOA/PlotResults.m
@@ -10,20 +10,17 @@ Sp = McLoadL([foldername '/SpectrumAtDetector.dat']);
 
 img = McLoadPSD([foldername '/AndorIkonL.dat']);
 
-figure,
-plot(MpB(:,1),MpB(:,2)./sum(MpB(:,2))), hold on
-% plot(PpB(:,1),PpB(:,2)./sum(PpB(:,2))), hold on
-plot(Sp(:,1),Sp(:,2)./sum(Sp(:,2))/0.023*0.00487), hold on
-grid
-legend('Mean Brilliance','Spectrum at iKonL')
-% legend('Mean Brilliance','Peak Brilliance','Spectrum at iKonL')
-% set(gca,'Yscale','log')%, ylim([1e5 1e18])
-
-figure, imagesc(pD), title('Divergence at pinhole')
-figure, imagesc(img), title('iKon-L image')
-
+figure, imagesc(linspace(-2,2,100),linspace(-2,2,100),pD), title('Divergence at pinhole')
+xlabel('degrees'), ylabel('degrees')
+figure, imagesc(linspace(-130/2,130/2,512),linspace(-130/2,130/2,512),imgaussfilt(img,3)), title('iKon-L image')
+xlabel('mm'), ylabel('mm')
 %%
 load BOA_spectrum.mat
 figure,
-plot(BOA(:,1),BOA(:,2)), hold on
+plot(MpB(:,1),MpB(:,2)./max(MpB(:,2))), hold on
 plot(Sp(:,1),Sp(:,2)./max(Sp(:,2))), hold on
+plot(BOA(:,1),BOA(:,2))
+grid
+legend('Mean Brilliance','Spectrum at iKonL','Measured')
+% legend('Mean Brilliance','Peak Brilliance','Spectrum at iKonL')
+% set(gca,'Yscale','log')%, ylim([1e5 1e18])
